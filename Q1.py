e32394a83f0289d76d435bd1eb954efaddd25b30
maulanaakbardj <asinmanis11@gmail@.com>
2021-10-27 14:29:43 +0700

    python

---
+++ Q1.py
@@ -0,0 +1,23 @@
+def f(a):
+    s = ""
+    i = 0
+    while i < len(a) - 1:
+        s = s + a[i+1]
+        i = i + 1
+    return s
+
+def g(a, b):
+    if b == 0 :
+        return a
+    return f(a) + a[0]
+
+s = "0123456789"
+i = 0
+while i < 7 :
+    j = 0
+    while j < 100000000 :
+        s = g(s, 9000000)
+        j = j +1
+    s = g(s,9)
+    i = i + 1
+print(s) 
---
+++ Q2.py
@@ -0,0 +1,10 @@
+def f (x, arr ): 
+  x= [5, 7, 8, 3]
+  i = 0
+  s = 0
+  while i < length(arr) :
+    s = s + arr[i]
+    i = i + 1
+  end
+  return s
+print(x)
