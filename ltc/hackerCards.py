a932deef147d34d48de4db9968203a7d87190e9d
Maulana Akbar Dwijaya <67249292+maulanaakbardj@users.noreply.github.com>
2022-01-13 23:15:08 +0700

    Update hackerCards.py

--- ltc/hackerCards.py
+++ ltc/hackerCards.py
@@ -1,3 +1,10 @@
+'''Q1: Birthday Card Collection
+HackerCards is a trendy new card game. Each type of HackerCard has a distinct ID number greater that or equals to 1, and the cost of each HackerCard equals its ID number. For example, HackerCard 1 costs 1, HackerCard 5 costs 5, and so on.
+
+Leanne already has a collection started. For her birthday, Mike wants to buy her as many cards as he can, but they must cost less than or equals to his budget. He wants to buy one each of some cards she doesn't already have. If he has to make one choice among several, he will always choose the lowest cost option. Determine which cards he will buy.
+
+For example, Leanne's collection = [2, 4, 5] and Mike has d = 7 to spend. He can purchase a maximum of 2 cards, the 1 and the 3 to add to her collection. Two other options he has are 1 and 6 (costs more) or 7(fewer cards, costs more)'''
+
 def hackerCards(collection, d):
     collection = set(collection)
     res = []
