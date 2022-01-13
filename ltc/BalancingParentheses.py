da23a4aff8c7d0bac31d70745c61874891f2060b
Maulana Akbar Dwijaya <67249292+maulanaakbardj@users.noreply.github.com>
2022-01-13 23:22:37 +0700

    Create BalancingParentheses.py

---
+++ ltc/BalancingParentheses.py
@@ -0,0 +1,12 @@
+def getMin(S):
+	balance = insertLeft = 0 
+        
+	for s in S:
+		if s == '(': balance += 1
+		else: balance -= 1
+		if balance == -1:
+		    insertLeft += 1
+			balance += 1
+			
+	# remaining balance is the extra left parentheses
+	return insertLeft + balance
