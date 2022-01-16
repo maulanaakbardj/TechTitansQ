1524be1f8ea9d1d586d84cd2c2303c75fd6b3841
Maulana Akbar Dwijaya <67249292+maulanaakbardj@users.noreply.github.com>
2022-01-16 13:25:25 +0700

    Create ContainerWithMostWater.py

---
+++ ltcproblem/ContainerWithMostWater.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(height) - 1
+        result = 0
+        while left < right:
+            result = max(min(height[left], height[right]) * (right - left), result)
+            if height[left] > height[right]:
+                # remove right
+                right -= 1
+            else:
+                # remove left
+                left += 1
+        return result
